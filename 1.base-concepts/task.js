813aebe5c00fcf41761e98dea722cadf31105a6a
Roman <CurajPompei@yandex.ru>
2025-01-16 20:59:30 +0300

    Решения задачи 1. Основные понятия

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    return [];
+  }
+  else if (d == 0) {
+    return [-b / (2 * a)];
+  }
+  else if (d > 0) {
+    return [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)]
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+  let p = percent / 100 / 12;
+  let s = amount - contribution;
+  let platej = s * (p + (p / (((1 + p) ** countMonths) - 1)));
+  let sum = platej * countMonths;
+  sum = Math.round(sum * 100) / 100;
+  return sum;
+} 
