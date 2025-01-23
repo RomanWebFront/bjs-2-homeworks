d8495dfa14ec46bbb6514926b60e579188a1ad1c
Roman <CurajPompei@yandex.ru>
2025-01-23 20:12:49 +0300

    Решение задачи 4. Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if ("marks" in this)
+        this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+    if ("marks" in this) {
+        if (this.marks.length === 0) {
+            return 0;
+        }
+        let sum = 0;
+        for (let a of this.marks)
+            sum += a;
+        return sum / this.marks.length;
+    } else
+        return 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
